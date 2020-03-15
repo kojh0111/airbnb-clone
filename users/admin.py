fb77e2b5f9435fa4c05514a3bbf7f06cbe773409
고정현 <kohj.h.@KOHs-MacBookPro.local>
2020-03-15 22:38:03 +0900

    Replacing Default User, Adding Model Fields

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+
+    """ Custom  User Admin """
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = ("superhost", "language")
+
