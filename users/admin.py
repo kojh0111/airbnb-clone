2d7b469c990bca8bdaae0f02cabaffb7b0979597
고정현 <kohj.h.@KOHs-MacBookPro.local>
2020-03-16 21:56:56 +0900

    Finishing User App

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
 
     """ Custom  User Admin """
 
-    list_display = ("username", "gender", "language", "currency", "superhost")
-    list_filter = ("superhost", "language")
-
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
