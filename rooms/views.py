e17521e7d7edc9884e0aad19c6a9906d7a2856a9
고정현 <kohj.h.@KOHs-MacBookPro.local>
2020-05-10 20:45:02 +0900

    Difference between get_page and page

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -7,8 +7,11 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms},)
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
